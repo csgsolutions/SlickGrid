53db1f3d97a6140c47f85d533eeb716bb53a51a8
Justin Buchanan <jbuch@buchmail.com>
2015-02-25 16:13:03 -0600

    Fix various bugs

--- plugins/slick.headerrowsummary.js
+++ plugins/slick.headerrowsummary.js
@@ -42,7 +42,7 @@
         function computeAggregates(data, columns) {
             var x;
             var y;
-            var length = data.length;
+            var length = data.getLength ? data.getLength() : data.length;
             var value;
             var $row;
             var $line;
@@ -52,7 +52,7 @@
             for (y = 0; y < length; y++) {
                 for (x = 0; x < columns.length; x++) {
                     if (columns[x].summaries && columns[x].summaries.length) {
-                        value = data[y][columns[x].field];
+                        value = data.getItem(y)[columns[x].field];
                         column = columns[x];
 
                         if (y === 0 || column.aggregates == null) {
@@ -71,7 +71,7 @@
 
                         if (typeof value === 'number') {
                             if (isNaN(column.aggregates.sum)) {
-                                column.aggregates.sum = 0;
+                                column.aggregates.sum = value;
                                 column.aggregates.min = value;
                                 column.aggregates.max = value;
                             } else {
@@ -110,31 +110,31 @@
 			if (column.summaries.indexOf('count') >= 0) {
 				$line = $('<li>').appendTo($list);
 				$('<label>').text(options.countLabel).appendTo($line);
-				$('<span>').text(column.aggregates.count.toString()).appendTo($line);
+				$('<span>').html(column.aggregates.count.toString()).appendTo($line);
 			}
 
 			if (column.summaries.indexOf('sum') >= 0 && !isNaN(column.aggregates.sum)) {
 				$line = $('<li>').appendTo($list);
 				$('<label>').text(options.sumLabel).appendTo($line);
-				$('<span>').text(formatter(-1, idx, column.aggregates.sum)).appendTo($line);
+				$('<span>').html(formatter(-1, idx, column.aggregates.sum)).appendTo($line);
 			}
 
 			if (column.summaries.indexOf('min') >= 0 && !isNaN(column.aggregates.min)) {
 				$line = $('<li>').appendTo($list);
 				$('<label>').text(options.minLabel).appendTo($line);
-				$('<span>').text(formatter(-1, idx, column.aggregates.min)).appendTo($line);
+				$('<span>').html(formatter(-1, idx, column.aggregates.min)).appendTo($line);
 			}
 
 			if (column.summaries.indexOf('max') >= 0 && !isNaN(column.aggregates.max)) {
 				$line = $('<li>').appendTo($list);
 				$('<label>').text(options.maxLabel).appendTo($line);
-				$('<span>').text(formatter(-1, idx, column.aggregates.max)).appendTo($line);
+				$('<span>').html(formatter(-1, idx, column.aggregates.max)).appendTo($line);
 			}
 
 			if (column.summaries.indexOf('avg') >= 0 && column.aggregates.count > 0 && !isNaN(column.aggregates.sum)) {
 				$line = $('<li>').appendTo($list);
 				$('<label>').text(options.avgLabel).appendTo($line);
-				$('<span>').text(formatter(-1, idx, column.aggregates.sum / column.aggregates.count)).appendTo($line);
+				$('<span>').html(formatter(-1, idx, column.aggregates.sum / column.aggregates.count)).appendTo($line);
 			}
         }
 	
@@ -142,7 +142,7 @@
             $(args.node).empty().data('columnID', args.column.id);
 
             if (args.column.aggregates == null) {
-                computeAggregates(dataView.getItems(), _grid.getColumns());
+                computeAggregates(dataView, _grid.getColumns());
             }
 
 			renderSummaries(args.column, args.node);
@@ -156,7 +156,7 @@
             _timeout = window.setTimeout(function () {
                 var cols = _grid.getColumns();
 				var row = _grid.getHeaderRow();
-                computeAggregates(dataView.getItems(), cols);
+                computeAggregates(dataView, cols);
                 renderAllSummaries(cols, row);
             }, 10);
         }
